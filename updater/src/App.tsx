5231c573443981d352d113569d433f63160f8b57
KyungHo Park <crorowslowpp@gmail.com>
2022-04-07 16:41:08 +0900

    Design: a little bit of design added

--- updater/src/App.tsx
+++ updater/src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] =  useState(0);
+
+  const handleClick = () => setCounter((prev) => prev + 1)
+
   return (
-    <div className="App">
-      <p> Hello </p>
+    <div style={{display: "flex", flexDirection:'column',margin: "40px",padding: "8px", width: "300px", alignItems:"center", backgroundColor:"teal"}}>
+      <React.Fragment>
+        <div style={{height: 80, width: 200, textAlign:"center"}}>I am an counter!</div>
+        <h3>counter : <span>{counter}</span></h3>
+        <button onClick={handleClick}> increment</button>
+      </React.Fragment>
     </div>
   );
 }
