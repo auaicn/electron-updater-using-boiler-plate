7cab537963f0fdcf2717e84030f68c62daec17ea
KyungHo Park <crorowslowpp@gmail.com>
2022-04-12 13:11:10 +0900

    Feat: added explicit version string

--- updater/src/App.tsx
+++ updater/src/App.tsx
@@ -6,8 +6,9 @@ function App() {
   const handleClick = () => setCounter((prev) => prev + 1)
 
   return (
-    <div style={{display: "flex", flexDirection:'column',margin: "40px",padding: "8px", width: "300px", alignItems:"center", backgroundColor:"blue"}}>
+    <div style={{display: "flex", flexDirection:'column',margin: "40px",padding: "8px", width: "300px", alignItems:"center", backgroundColor:"orange"}}>
       <React.Fragment>
+        <h1> 직접 적어준 버전 1.0.3</h1>
         <div style={{height: 80, width: 200, textAlign:"center"}}>I am an counter!</div>
         <h3>counter : <span>{counter}</span></h3>
         <button onClick={handleClick}> increment</button>
